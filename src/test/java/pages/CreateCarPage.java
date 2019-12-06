dcd6c5c1c12e5b665d0024a5d3cf32feee41bb74
sabir mustafayev <sabirmustafayev86@gmail.com>
2019-12-06 16:34:56 -0500

    Some elements added

--- src/test/java/pages/CreateCarPage.java
+++ src/test/java/pages/CreateCarPage.java
@@ -10,12 +10,25 @@ public class CreateCarPage extends BasePage {
 
     @FindBy(css = "[id^='custom_entity_type_LicensePlate']")
     public WebElement licensePlateElement;
+    @FindBy(name = "custom_entity_type[Driver]")
+    public WebElement driverElement;
+    @FindBy(name = "custom_entity_type[Location]")
+    public WebElement locationElement;
+    @FindBy(xpath = "//div[@class='btn-group pull-right open']/button[contains(text(),'Save and Close')]")
+    public WebElement saveAndCloseButtonElement;
+    @FindBy(css = "div[id*='FuelType']")
+    public WebElement fuelTypeElement;
     /**
      * This method stands for selecting tags
      * Provide tag name to select
      * If checkbox already selected, it will not do anything
-     * @param tagName
+     *
+     * @param tagName - represents name of tag that has to be selected, like Junior, Senior or Purchased
      * @return webelement of checkbox that was selected
+     * <p>
+     * CreateCarPage createCarPage = new CreateCarPage();
+     * <p>
+     * createCarPage.selectTags("Senior"); // Senior tag will be selected
      */
     public WebElement selectTags(String tagName) {
         //locator for checkbox is based on label name
@@ -28,5 +41,23 @@ public class CreateCarPage extends BasePage {
             checkBox.click();
         }
         return checkBox;
+    }
+    /**
+     * Select fuel type by visible text
+     *
+     * @param fuelType - Diesel, Electric, Hybrid
+     *  <p>
+     *  usage: CreateCarPage createCarPage = new CreateCarPage();
+     *  <p>
+     *  to select gasoline type
+     *  createCarPage.selectFuelType("Diesel"); - if you want to select Diesel as fuel type
+     */
+    public void selectFuelType(String fuelType) {
+        String locator = "//div[@class='select2-result-label' and text()='" + fuelType + "']";
+        BrowserUtils.waitForClickablility(fuelTypeElement, 15);
+        fuelTypeElement.click();
+        WebElement fuelTypeSelectionElement = Driver.get().findElement(By.xpath(locator));
+        BrowserUtils.waitForClickablility(fuelTypeSelectionElement, 15);
+        fuelTypeSelectionElement.click();
     }
 }
