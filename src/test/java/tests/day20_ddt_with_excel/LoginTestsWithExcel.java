41a85d26476d1dfbc93062ee4c0fd6327beaf00a
sabir mustafayev <sabirmustafayev86@gmail.com>
2019-12-10 17:08:24 -0500

    loginTest Test added to LoginTestsWithExcel class

--- src/test/java/tests/day20_ddt_with_excel/LoginTestsWithExcel.java
+++ src/test/java/tests/day20_ddt_with_excel/LoginTestsWithExcel.java
@@ -1,39 +1,40 @@
 package tests.day20_ddt_with_excel;
 
+import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import pages.LoginPage;
 import tests.TestBase;
+import utils.Driver;
 import utils.ExcelUtil;
-
 public class LoginTestsWithExcel extends TestBase {
-
-    @Test(dataProvider = "credentials",description = "Login")
-    public void loginTest(){
-
+    //username  password    firstname   lastname    result
+    @Test(dataProvider = "credentials", description = "Login with different credentials")
+    public void loginTest(String username, String password, String firstName, String lastName, String result) {
+        //is must because we will ge null pointer exception
+        extentTest = extentReports.createTest("Login as "+username);
+        if(username.equals("username")){
+            //will make test skipped
+            //it will not fail
+            //because first row is dedicated to column names
+            throw new SkipException("Test was skipped because it's first row!");
+        }else{
+            LoginPage loginPage = new LoginPage();
+            loginPage.login(username, password);
+            //put here wait for title to be "Dashboard"
+            Assert.assertEquals(Driver.get().getTitle(), "Dashboard");
+            extentTest.pass("Login test passed for user "+username);
+        }
     }
-
-
-
-
-
-
-
-
     //is a test data supplier
     //as many sets of data it returns
     //as many times exactly same test will run
     @DataProvider(name = "credentials")
-    public static Object[][] credentials(){
+    public static Object[][] credentials() {
         ExcelUtil qa2 = new ExcelUtil("vytrack_testusers.xlsx", "QA2-short");
         return qa2.getDataArray();
     }
-
-
-
-
-
-
-
 //    public static void main(String[] args) {
 //        ExcelUtil qa2 = new ExcelUtil("vytrack_testusers.xlsx", "QA2-short");
 //        System.out.println("Row count: " + qa2.rowCount());
@@ -48,6 +49,4 @@ public class LoginTestsWithExcel extends TestBase {
 //            System.out.println(map.get("username"));
 //        }
 //    }
-
-
 }
